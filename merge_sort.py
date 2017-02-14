013d38387f3ae498cafb8c823bb68b134d26b23e
William W Wold <wilcoe@rocketmail.com>
2017-02-14 14:15:57 -0800

    implemented merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,4 +1,37 @@
 
-def sort(array):
-	return array
+def merge(array0, array1):
+	i0 = 0
+	i1 = 0
+	out = []
+	
+	while (True):
+		if i0<len(array0) and i1<len(array1):
+			if (array0[i0]<array1[i1]):
+				out.append(array0[i0])
+				i0+=1
+			else:
+				out.append(array1[i1])
+				i1+=1
+		elif i0<len(array0):
+			out.append(array0[i0])
+			i0+=1
+		elif i1<len(array1):
+			out.append(array1[i1])
+			i1+=1
+		else:
+			break
+	
+	return out
 
+def sort_level(array, left, right):
+	if (left+1>=right):
+		return [array[left]]
+	else:
+		middle=int((right-left)/2)+left
+		array0=sort_level(array, left, middle)
+		array1=sort_level(array, middle, right)
+		return merge(array0, array1)
+
+def sort(array):
+	return sort_level(array, 0, len(array))
+	
