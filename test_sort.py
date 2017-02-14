b01b3983c670b26c833e2121d9b4607242cbd602
William W Wold <wilcoe@rocketmail.com>
2017-02-14 13:56:02 -0800

    wrote unit test

---
+++ test_sort.py
@@ -0,0 +1,13 @@
+from merge_sort import sort
+import unittest
+
+class SortTest(unittest.TestCase):
+	def test_simple_cases(self):
+		assert sort([5, 4, 1, 2, 3]) == [1, 2, 3, 4, 5]
+		assert sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+		assert sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+		assert sort([2, 3, 4, 1]) == [1, 2, 3, 4]
+
+if __name__ == '__main__':
+    unittest.main()
+    
