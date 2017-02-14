58d9df3a67e462e195d5bd6fbe3ee148cbfab4bd
William W Wold <wilcoe@rocketmail.com>
2017-02-14 13:37:49 -0800

    changed 2-3 tree

--- 2_3_tree.py
+++ 2_3_tree.py
@@ -2,13 +2,15 @@
 class Tree23:
 	def __init__(self):
 		self.root = None
-	
+
 	def insert(self, item):
 		if self.root == None:
 			self.root = Node23(item)
 		else:
-			self.root.insert(item)
-	
+			new = self.root.insert(item)
+			if new != None:
+				self.root = new
+
 	def searchFor(self, item):
 		if self.root == None:
 			return False
@@ -22,16 +24,16 @@ class Node23:
 		self.leftSub = None
 		self.midSub = None
 		self.rightSub = None
-	
+
 	def __init__(self, leftIn, val, rightIn):
 		self.a = val
 		self.b = None
 		self.leftSub = leftIn
 		self.midSub = None
 		self.rightSub = rightIn
-	
+
 	def insert(self, item):
-		if self.leftSub == None:
+		if self.leftSub == None: # if this node is a leaf
 			if self.b == None:
 				if item < self.a:
 					self.b = self.a
@@ -45,7 +47,7 @@ class Node23:
 					return Node23(Node23(self.a), item, Node23(self.b))
 				else:
 					return Node23(Node23(self.a), self.b, Node23(item))
-		else:
+		else: # If this node is not a leaf
 			if item < self.a:
 				new = self.leftSub.insert(item)
 				if new != None:
@@ -53,7 +55,7 @@ class Node23:
 						self.b = self.a
 						self.a = new.val
 						self.leftSub = new.leftSub
-						self.midSum = new.rightSub
+						self.midSub = new.rightSub
 					else:
 						return Node23(new, self.a, Node23(self.midSub, self.b, self.rightSub))
 			else:
@@ -75,36 +77,22 @@ class Node23:
 						new = self.rightSub.insert(item)
 						return Node23(Node23(self.leftSub, self.a, self.rightSub), self.b, new)
 
-	
+
 	def searchFor(self, item):
-		if self.a == item:
+		if self.a == item or self.b == item:
 			return True
 		elif item < self.a:
 			if self.leftSub == None:
 				return False
 			else:
 				return self.leftSub.searchFor(Item)
-		elif self.b == None:
+		elif self.b == None or item > self.b:
 			if self.rightSub == None:
 				return False
 			else:
 				return self.rightSub.searchFor(item)
-		elif self.b == item:
-			return True
-		elif item < self.b:
+		else:
 			if self.midSub == None:
 				return False
 			else:
 				return self.midSub.searchFor(item)
-		else:
-			if self.rightSub == None:
-				return False
-			else:
-				return self.rightSub.searchFor(item)
-		
-
-
-
-
-
-
